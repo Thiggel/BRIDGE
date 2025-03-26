3dc0ff1baa49b0440904f3814acc0ccbb42c421e
Thiggel <f.laitenberger@gmail.com>
2025-03-26 11:16:27 +0100

    adjusted config

--- bridge_pretrain.py
+++ bridge_pretrain.py
@@ -262,7 +262,7 @@ def main(cfg: DictConfig):
     #   3.4. change dataset path in cfg to the new dataset
     # 4. repeat from (1) until num_cycles is reached
     for cycle_idx in range(cfg.bridge.num_cycles):
-        print(cfg.to_yaml())
+        print(OmegaConf.to_yaml(cfg))
         print(cfg.augmentations.rrc)
         exit()
         print(f"Cycle {cycle_idx + 1}/{cfg.bridge.num_cycles}")
